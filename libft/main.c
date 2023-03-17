3fab745b39c864c31f485684030c96f3a8da589f
Hoseong Song <hoseoson@c8r8s2.42seoul.kr>
2023-03-18 00:09:33 +0900

    save

--- libft/main.c
+++ libft/main.c
@@ -7,8 +7,19 @@ int main()
 
 	char *str;
 	str = 0;
-	printf("%c", *str);
+	// strnstr("fake", ((void *)0), 3);
 	// printf("%s", strnstr((void *)0, "fake", 0));
+	// printf("%s\n", ft_strnstr((void *)0, "fake", 0));
+	// printf("%s\n", strnstr(((void *)0), "fake", 3));
+	// printf("%s\n", ft_strnstr(((void *)0), "fake", 3));
+	
 	// ft_strnstr(((void *)0), "fake", 0);
+
+	char *s1 = "oh no not the empty string !";
+	char *s2 = "";
+	size_t max = 0;
+	char *i1 = strnstr(s1, s2, max);
+	char *i2 = ft_strnstr(s1, s2, max);
+	printf("%s, %s",i1, i2);
 
 }
