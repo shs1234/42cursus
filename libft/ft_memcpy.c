f684b63bbfb3d123bea6e5ac23c99b7f986f1d29
Hoseong Song <hoseoson@c4r8s3.42seoul.kr>
2023-03-27 14:05:33 +0900

    lib

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hoseoson <hoseoson@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 11:49:39 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/03/23 04:16:17 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/03/27 14:01:34 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*uc_src;
 	unsigned char	*uc_dst;
 
+	if (!dst && !src)
+		return (0);
 	uc_dst = (unsigned char *)dst;
 	uc_src = (unsigned char *)src;
-	if (!dst || !src)
-		return (0);
 	while (n--)
 		*uc_dst++ = *uc_src++;
 	return (dst);
