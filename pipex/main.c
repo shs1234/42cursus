b6fab5089b220cb177c43adfcbf2b6be75f04fa6
Hoseong Song <hoseoson@c3r7s6.42seoul.kr>
2023-05-08 07:16:00 +0900

    asd

--- pipex/main.c
+++ pipex/main.c
@@ -6,17 +6,42 @@
 /*   By: hoseoson <hoseoson@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 00:51:57 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/04/30 01:27:23 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/05/08 06:57:16 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+static int	ft_valid_outfile(int ac, char **av)
+{
+	char	*last_slash;
+	char	*outfile;
+
+	outfile = ft_strdup(av[ac - 1]);
+	last_slash = ft_strrchr(outfile, '/');
+	if (last_slash != NULL)
+	{
+		*last_slash = '\0';
+		if (access(outfile, F_OK) == -1)
+		{
+			free(outfile);
+			ft_printf("directory\n");
+			return (0);
+		}
+	}
+	free(outfile);
+	return (1);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	if (ac == 5)
-		ft_pipex(av, envp);
+	{
+		if (!ft_valid_outfile(ac, av))
+			return (1);
+		ft_pipex(ac, av, envp);
+	}
 	else
-		exit(1);
+		return (1);
 	return (0);
 }
--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,22 +6,22 @@
 /*   By: hoseoson <hoseoson@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 07:04:21 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/05/06 02:09:34 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/05/08 06:55:43 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_info_init(char **av, char **envp, t_info *info)
+void	ft_info_init(int ac, char **av, char **envp, t_info *info)
 {
 	char	**path;
 
 	if (pipe(info->pipe) == -1)
-		ft_error("pipe");
+		ft_error(strerror(errno));
 	info->infile_fd = open(av[1], O_RDONLY);
-	info->outfile_fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	info->outfile_fd = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (info->infile_fd < 0 || info->outfile_fd < 0)
-		ft_printf("%s", "No such file or directory");
+		ft_printf("FD");
 	while (ft_strncmp(*envp, "PATH", 4))
 		envp++;
 	path = ft_split(&(*envp)[5], ':');
@@ -43,8 +43,8 @@ void	ft_sub_process(pid_t *pid, t_info *info, char **envp)
 		dup2(info->infile_fd, STDIN_FILENO);
 		dup2(info->pipe[1], STDOUT_FILENO);
 		ft_closepipe(info->pipe);
-		execve(info->pathcmd1, info->cmd1_split, envp);
-		exit(0);
+		if (execve(info->pathcmd1, info->cmd1_split, envp) == -1)
+			exit(errno);
 	}
 	pid[1] = fork();
 	if (pid[1] < 0)
@@ -54,22 +54,22 @@ void	ft_sub_process(pid_t *pid, t_info *info, char **envp)
 		dup2(info->outfile_fd, STDOUT_FILENO);
 		dup2(info->pipe[0], STDIN_FILENO);
 		ft_closepipe(info->pipe);
-		// if (!ft_strchr(info->pathcmd2, '/'))
-		// 	exit(127);
-		execve(info->pathcmd2, info->cmd2_split, envp);
+		if (!ft_strchr(info->pathcmd2, '/'))
+			exit(127);
+		if (execve(info->pathcmd2, info->cmd2_split, envp) == -1)
+			exit(errno);
 	}
 }
 
-void	ft_pipex(char **av, char **envp)
+void	ft_pipex(int ac, char **av, char **envp)
 {
 	t_info	info;
 	pid_t	pid[2];
 	int		status;
 
-	ft_info_init(av, envp, &info);
+	ft_info_init(ac, av, envp, &info);
 	ft_sub_process(pid, &info, envp);
 	ft_closepipe(info.pipe);
 	waitpid(pid[1], &status, 0);
-	if (WIFEXITED(status))
-		exit(WEXITSTATUS(status));
+	exit(WEXITSTATUS(status));
 }
