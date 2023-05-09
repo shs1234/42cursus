71b26af45f79f7ad845269b2844faa3af7d3613e
Hoseong Song <hoseoson@c3r4s6.42seoul.kr>
2023-05-10 07:01:37 +0900

    asd

--- so_long/main.c
+++ so_long/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoseoson <hoseoson@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: hoseoson <hoseoson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:33:27 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/05/09 23:36:02 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/05/10 06:10:56 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(int ac, char **av)
 {
+	void *mlx_ptr;
+	void *win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	mlx_loop(mlx_ptr);
 	return (0);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,15 +6,15 @@
 /*   By: hoseoson <hoseoson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:33:19 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/05/10 05:15:22 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/05/10 06:45:22 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "ft_printf/ft_printf.h"
 # include "libft/libft.h"
+# include "minilibx/mlx.h"
 # include "math.h"
 
 #endif
