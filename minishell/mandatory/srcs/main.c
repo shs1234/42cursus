d609d908cf94af2911cf35d3d8f5b96f9e4aa736
shs <guichanist@gmail.com>
2023-06-12 04:38:31 +0900

    asd

--- minishell/mandatory/srcs/main.c
+++ minishell/mandatory/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hoseoson <hoseoson@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 03:20:39 by hoseoson          #+#    #+#             */
-/*   Updated: 2023/06/11 06:23:05 by hoseoson         ###   ########.fr       */
+/*   Updated: 2023/06/11 06:33:45 by hoseoson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,8 @@ int	main(int ac, char **av, char **envp)
 	(void)envp;
 	return (0);
 }
+// https://runebook.dev/ko/docs/bash/-index-
+// https://www.gnu.org/software/bash/manual/bash.html
+
+// https://cdn.intra.42.fr/pdf/pdf/68660/en.subject.pdf
+// https://github.com/42seoul-translation/subject_ko/blob/master/minishell/minishell.md
