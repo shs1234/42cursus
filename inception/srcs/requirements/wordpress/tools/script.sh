669abbab8fead3504c4b72edd968f2cfaf321837
shs <shs@shs.shs>
2024-04-12 18:59:06 +0000

    save

---
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,11 @@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+mkdir /var/www/
+mkdir /var/www/html
+cd /var/www/html
+
+wp core download --allow-root
+
+mv /wp-config.php /var/www/html/wp-config.php
