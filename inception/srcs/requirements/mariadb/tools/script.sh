62d947b52fa306dd257fda85e784c0d24d3287b7
shs <shs@shs.shs>
2024-04-17 15:10:55 +0000

    save

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,3 +1,19 @@
-service mariadb start
-service mariadb stop
+#!/bin/bash
+
+# service mariadb start
+# service mariadb stop
+# mysqld
+service mysql start 
+
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
 mysqld
--- inception/srcs/requirements/nginx/tools/script.sh
+++ inception/srcs/requirements/nginx/tools/script.sh
@@ -1,4 +1,9 @@
 mv /nginx.conf /etc/nginx/
 mv /default /etc/nginx/sites-available/
 
+openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr -subj "/C=KR/L=SEOUL/O=42seoul"
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+mv server.crt /etc/ssl/certs/
+mv server.key /etc/ssl/private/
+
 nginx -g 'daemon off;'
