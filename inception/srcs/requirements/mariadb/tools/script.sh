888b06611ec5dbdf1f6bc437f8a9dc5389f0facc
shs <shs@shs.shs>
2024-04-22 22:49:24 +0000

    save

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,13 @@
 # !/bin/bash
-
-# export DB_NAME=wordpress
-# export DB_USER=user
-# export DB_PASSWORD=password
-
+DB_NAME=wordpress
+DB_USER=user
+DB_PASSWORD=password
 service mariadb start
+sleep 3
 
-echo "CREATE DATABASE $DB_NAME default CHARACTER SET UTF8;" | mysql
-echo "CREATE USER '$DB_USER'@'%' identified by '$DB_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';" | mysql
+echo "CREATE DATABASE $DB_NAME;" | mysql
+echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
 
 service mariadb stop
